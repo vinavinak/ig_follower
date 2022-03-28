cd84a03a1231b9754e60cd4cb01a839f7112ce0c
iNoCiWWlKv <vinaknkt@gmail.com>
2022-03-28 21:26:16 +0800

    test: add CI/CD automation flow. fixed #11

---
+++ tests/test_ci.py
@@ -0,0 +1,28 @@
+"""
+A complete end-to-end test for an e-commerce website.
+"""
+from seleniumbase import BaseCase
+
+
+class MyTestClass(BaseCase):
+    def test_swag_labs(self):
+        self.open("https://www.saucedemo.com")
+        self.type("#user-name", "standard_user")
+        self.type("#password", "secret_sauce\n")
+        self.assert_element("#inventory_container")
+        self.assert_text("PRODUCTS", "span.title")
+        self.click('button[name*="backpack"]')
+        self.click("#shopping_cart_container a")
+        self.assert_text("YOUR CART", "span.title")
+        self.assert_text("Backpack", "div.cart_item")
+        self.click("button#checkout")
+        self.type("#first-name", "SeleniumBase")
+        self.type("#last-name", "Automation")
+        self.type("#postal-code", "77123")
+        self.click("input#continue")
+        self.assert_text("CHECKOUT: OVERVIEW")
+        self.assert_text("Backpack", "div.cart_item")
+        self.click("button#finish")
+        self.assert_exact_text("THANK YOU FOR YOUR ORDER", "h2")
+        self.assert_element('img[alt="Pony Express"]')
+        self.js_click("a#logout_sidebar_link")
